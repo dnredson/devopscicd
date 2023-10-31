c40166bc79e8dc91106a660e450b4ed08182b98f
dnredson <dnredson@gmail.com>
2023-10-31 10:45:48 -0300

    Add files via upload

---
+++ CICD/completo/main.py
@@ -0,0 +1,5 @@
+def soma(x, y):
+    print("Somando os valores x e y")
+    soma = x + y
+    print("Soma dentro da função: "+soma)
+    return soma
---
+++ CICD/completo/teste.py
@@ -0,0 +1,4 @@
+from soma import main
+
+resultado = soma(10, 15)
+print(f"Exibindo resultado da soma no teste: {resultado}")
